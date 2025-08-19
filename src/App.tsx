ee79193a59a8c1736409dabe2bddc9286ba94bbd
deluxe-wizard-seven <diptobhattacharjee6@gmail.com>
2025-08-19 13:11:35 +0530

    fixed the event type of handleInputChange function.

--- src/App.tsx
+++ src/App.tsx
@@ -131,7 +131,9 @@ const App = () => {
     message: "",
   });
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
     const { name, value } = e.target;
     setFormData((prev) => ({
       ...prev,
